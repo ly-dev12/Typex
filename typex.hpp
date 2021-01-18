33af74c9d938bba702f95b1e132fd67ce6afeb58
lucho1996g <lucho1996g@gmail.com>
2021-01-17 21:39:34 -0500

    mytyper

---
+++ typex.hpp
@@ -0,0 +1,12 @@
+
+#ifndef TYPEX_H
+    #define TYPEX_H
+    
+    typedef unsigned short int uint16;
+    typedef signed short int int16;
+    typedef unsigned long uint32;
+    typedef signed long int32;
+    typedef unsigned long long uint64;
+    typedef signed long long int64;
+    typedef char ichar;
+#endif
